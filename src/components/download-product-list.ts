2e5baa7c2c15ad86b033e9d4973ad93dba164df4
Christian Wiggert <wiggert@bryter.io>
2024-08-07 22:38:47 +0200

    Use API pagination

--- src/components/download-product-list.ts
+++ src/components/download-product-list.ts
@@ -2,10 +2,8 @@ import type { Product } from './types.ts'
 
 export const downloadProductList = async (): Promise<Product[]> => {
 	try {
-		const json = (await fetch('https://dummyjson.com/products')).json()
-		if (Array.isArray(json)) {
-			return json.slice(0, 100)
-		}
+		const result = await (await fetch('https://dummyjson.com/products?limit=100')).json()
+		return result.products
 	} catch (error) {
 		console.error(error)
 	}
