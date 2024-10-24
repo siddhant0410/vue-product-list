c6d68c26b7172742bdc72cf4127ff8fdeda7047e
siddhant0410 <siddhantbhadsavale@gmail.com>
2024-10-24 13:54:06 +0200

    Pagination Feature

---
+++ src/components/PaginationComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="pagination-controls text-center mt-4">
+    <button
+      @click="prevPage"
+      :disabled="currentPage === 1"
+      class="px-4 py-2 bg-gray-200 rounded-md"
+    >
+      Previous
+    </button>
+
+    <span class="mx-4">Page {{ currentPage }} of {{ totalPages }}</span>
+
+    <button
+      @click="nextPage"
+      :disabled="currentPage === totalPages"
+      class="px-4 py-2 bg-gray-200 rounded-md"
+    >
+      Next
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  currentPage: {
+    type: Number,
+    required: true,
+  },
+  totalPages: {
+    type: Number,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["pageChange"]);
+
+const nextPage = () => {
+  if (props.currentPage < props.totalPages) {
+    emit("pageChange", props.currentPage + 1);
+  }
+};
+
+const prevPage = () => {
+  if (props.currentPage > 1) {
+    emit("pageChange", props.currentPage - 1);
+  }
+};
+</script>
